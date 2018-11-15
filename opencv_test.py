75f764c6f2cfc4d34a6131cc0f20cfa336eec4a8
eufmike <sc2.shih@gmail.com>
2018-11-14 22:57:50 -0600

    test

--- opencv_test.py
+++ opencv_test.py
@@ -1,7 +1,37 @@
 # %%
 from PIL import Image
+# from pylab import *
 import os
+# import numpy as np
+
+
 pil_im = Image.open('pikachu.png')
 
-pil_im.convert('L').save('pikachu_1.png')
+# pil_im.convert('L').save('pikachu_1.png')
 
+im = np.array(pil_im.convert('L'))
+print(im.dtype)
+print(im.shape)
+
+'''
+im = np.array(pil_im.convert('L'), dtype = 'float64')
+print(im.dtype)
+print(im.shape)
+'''
+print(im.flatten())
+nbr_bins = 256
+imhist,bins = histogram(im.flatten(),nbr_bins,normed=True)
+print(imhist.shape)
+cdf = imhist.cumsum()
+cdf = 255 * cdf / cdf[-1]
+print(cdf.shape)
+print(bins[:-1])
+
+im2 = interp(im.flatten(),bins[:-1],cdf)
+
+
+
+# %%
+figure() 
+hist(im.flatten(),128) 
+show()
